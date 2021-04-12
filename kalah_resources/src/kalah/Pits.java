e5034faddc7bb0db12ce487790fc408d82f0eb34
Suraj Kumar <48311309+skmu104@users.noreply.github.com>
2021-04-12 15:58:48 +1200

    Add files via upload

---
+++ kalah_resources/src/kalah/Pits.java
@@ -0,0 +1,13 @@
+package kalah;
+
+public class Pits {//Parent class
+    public int seeds ;
+
+    public int getSeeds() {
+        return seeds;
+    }
+
+    public void setSeeds(int seeds) {
+        this.seeds = seeds;
+    }
+}
